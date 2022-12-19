7c1365eb273214e98a93d6c75fa2a41b689c188a
unknown <Charlotte Landuyt>
2022-12-19 11:06:17 +0100

    exo

---
+++ public/js/javascript.js
@@ -0,0 +1,47 @@
+//Exo1
+
+let exo1 = "J'aime les tartines et j'en mange ";
+
+console.log(exo1);
+
+
+let day = "4 par jours";
+let concatenation= exo1 + day;
+
+console.log(concatenation);
+
+
+let number1 = 5;
+let number2 = 35;
+
+let somme = number1 + number2;
+console.log(somme);
+
+
+let phrase_finale = exo1 + number1;
+console.log(phrase_finale);
+
+
+//Exo2
+
+let coucou = "Salut tout le monde";
+console.log(coucou);
+
+let bye = "Au revoir !";
+console.log(bye);
+
+coucou = bye;
+
+console.log(coucou);
+
+//exo3
+
+let number = 4;
+let string = "6";
+
+number > string;
+number < string;
+number == string;
+number != string;
+number === string;
+number !== string;
